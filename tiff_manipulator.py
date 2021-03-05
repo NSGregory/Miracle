33e4fe287b0a943ec1c294364a91c31bea3f8b8f
nsg-lab-remote <ngregory@stanford.edu>
2021-03-04 19:13:10 -0800

    VNC local

--- tiff_manipulator.py
+++ tiff_manipulator.py
@@ -1,6 +1,7 @@
 import cv2
 import pandas as pd
 #import matplotlib.pyplot as plt
+#pyplot throws error related to incompatibility with system install
 
 good_file = "/SSD2/Nick_G/KNK_sample2_decent/190808_mouse1L_Rhemi_488_16-50-08/16-50-08_Mouse1L_UltraII_C00_xyz-Table Z0642.ome.tif"
 
